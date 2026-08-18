c0280e052abd5f97bb8cb9210512e3a85ba5b61d
ShifoNavbat <admin@shifonavbat.uz>
2026-08-19 01:43:28 +0500

    fix: force SW update and improve push event handling in sw.js

--- app.js
+++ app.js
@@ -81,6 +81,7 @@ async function initServiceWorker() {
   if ('serviceWorker' in navigator) {
     const reg = await navigator.serviceWorker.register('/sw.js');
     console.log('✅ SW ro\'yxatdan o\'tdi');
+    reg.update();
     return reg;
   }
 }
